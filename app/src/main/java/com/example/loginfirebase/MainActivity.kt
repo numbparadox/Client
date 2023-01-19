a7508a24e65a19f652924bac28c98a947cbd64d2
Vaashkar Paul <vaashkarpaul2050@gmail.com>
2023-01-20 01:34:22 +0530

    Commit 1.1

--- app/src/main/java/com/example/loginfirebase/MainActivity.kt
+++ app/src/main/java/com/example/loginfirebase/MainActivity.kt
@@ -2,11 +2,50 @@ package com.example.loginfirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.loginfirebase.R
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.loginfirebase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        populateVehicles()
+
+        val mainActivity = this
+        binding.recyclerview.apply {
+            layoutManager = GridLayoutManager(applicationContext, 1)
+            adapter = CardAdapter(vehicleList)
+        }
+
+    }
+
+    private fun populateVehicles() {
+        val vehicle1 = Vehicles(
+            R.drawable.splendor,
+            "Splendor",
+            "Available 2"
+        )
+        vehicleList.add(vehicle1)
+
+        val vehicle2 = Vehicles(
+            R.drawable.duke,
+            "Duke",
+            "Available 2"
+        )
+        vehicleList.add(vehicle2)
+
+        val vehicle3 = Vehicles(
+            R.drawable.enfield,
+            "Royal Enfield",
+            "Available 2"
+        )
+        vehicleList.add(vehicle3)
+
+        vehicleList.add(vehicle1)
+        vehicleList.add(vehicle2)
+        vehicleList.add(vehicle3)
     }
 }
